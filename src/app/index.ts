cfcc46bf5db230dd280aa3d0780a14870aeb9fcc
Humberd <sawik11@wp.pl>
2017-04-30 15:49:58 +0200

    Removed trash

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,20 +2,14 @@ import "rxjs";
 import { TopBarController } from "./views/TopBar/TopBarController";
 import { HttpService } from "./http/HttpService";
 import { PageGiveawaysRetriever } from "./GiveawaysEnterer/PageGiveawaysRetriever";
-import { Observable } from "rxjs/Observable";
-import { EntererContext } from "./state/EntererContext";
 import { CouponsController } from "./views/Coupon/CouponsController";
-import { AjaxRequest } from "rxjs/Rx";
-import { Giveaway } from "./models/Giveaway";
-import { IdleState } from "./state/states/IdleState";
 require("./styles.scss");
 
 export class Main {
-  private topBar: TopBarController;
-  private coupons: CouponsController;
-  private http: HttpService;
+  private topBarController: TopBarController;
+  private couponsController: CouponsController;
+  private httpService: HttpService;
   private giveawaysRetriever: PageGiveawaysRetriever;
-  private entererContext: EntererContext;
 
   constructor() {
   }
@@ -23,18 +17,13 @@ export class Main {
   public init(): void {
     this.instantiateObjects();
     this.bindCallbacks();
-    this.idleContext();
   }
 
   private instantiateObjects(): void {
-    this.topBar = new TopBarController();
-    this.coupons = new CouponsController();
-    this.http = new HttpService();
-    this.giveawaysRetriever = new PageGiveawaysRetriever(this.http);
-    this.entererContext = new EntererContext(
-        this.giveawaysRetriever,
-        this.topBar,
-        this.coupons);
+    this.topBarController = new TopBarController();
+    this.couponsController = new CouponsController();
+    this.httpService = new HttpService();
+    this.giveawaysRetriever = new PageGiveawaysRetriever(this.httpService);
   }
 
   private bindCallbacks(): void {
@@ -43,19 +32,12 @@ export class Main {
     //     .subscribe((item: JQuery) => item.click());
 
 
-    Observable.fromEvent(this.coupons.getCoupons(), "click")
-        .map((item: JQueryEventObject) => $(item.currentTarget))
-        .map((item: JQuery) => item.parents(".tickets-col"))
-        .map((item: JQuery) => this.giveawaysRetriever.getGiveaway(item))
-        .do((item: Giveaway) => console.log(item))
-        .subscribe((item) => this.entererContext.startEntering([item]));
-  }
-
-  private idleContext(): void {
-    this.entererContext.changeCurrentState(new IdleState(
-        this.giveawaysRetriever,
-        this.topBar,
-        this.coupons));
+    // Observable.fromEvent(this.coupons.getCoupons(), "click")
+    //     .map((item: JQueryEventObject) => $(item.currentTarget))
+    //     .map((item: JQuery) => item.parents(".tickets-col"))
+    //     .map((item: JQuery) => this.giveawaysRetriever.getGiveaway(item))
+    //     .do((item: Giveaway) => console.log(item))
+    //     .subscribe((item) => this.entererContext.startEntering([item]));
   }
 }
 
--- src/app/state/State.ts
+++
@@ -1,8 +0,0 @@
-import { EntererContext } from "./EntererContext";
-import { Giveaway } from "../models/Giveaway";
-
-export interface State {
-  start(giveaways: Giveaway[], entererContext: EntererContext): void;
-  stop(entererContext: EntererContext): void;
-  getName(): string;
-}
